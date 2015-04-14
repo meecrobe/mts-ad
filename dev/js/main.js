1fc544017d0f56e85648f8587b52c82f9b5dfac9
Vadzim <meecrobe@gmail.com>
2015-04-15 00:42:28 +0300

    Fixes

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,27 +1,20 @@
 $(document).ready(function(){
-  $('.slide').hide();
+
   var timer;
+
   $('.anchor').on('click', function(){
     $('.slider-wrapper').fadeIn(function(){
       $('.popup-circle').show().addClass('animated zoomIn');
     });
     timer = setTimeout(function(){
       $('.slider-wrapper').hide();
-      $('.slide').children().removeClass('animated');
-      $('.slide').hide();
-    }, 30000);
-  });
-
-  $('video').on('play', function(){
-    clearTimeout(timer);
+      resetAll();
+    }, 120000);
   });
 
   $('.slider-wrapper').on('click', function(){
     $(this).hide();
-    $('video')[0].pause();
-    clearTimeout(timer);
-    $('.slide').hide();
-    $('.slide').children().removeClass('animated');
+    resetAll();
   });
 
   $('.popup-circle').on('click', function(event){
@@ -29,90 +22,99 @@ $(document).ready(function(){
   });
 
   $('.a-1').on('click', function(){
-
-    setTimeout(function() {
-      var slide = $('#slide-1');
-      slide.show().children().css({'visibility':'hidden'}).removeClass('animated');
-      slide.find('h1').css({'visibility':'visible'}).addClass('animated bounceIn');
-      setTimeout(function(){
-        slide.find('h1 + p, img.large-img').css({'visibility':'visible'}).addClass('animated bounceIn');
-       setTimeout(function(){
-        slide.find('h2').css({'visibility':'visible'}).addClass('animated bounceInUp');
-        $('.minsk, .lib').css({'visibility':'visible'}).addClass('animated bounceInUp');
-        }, 5000);
-      }, 3000);
-    }, 1600);
+    showSlide1();
   });
 
   $('.a-2').on('click', function(){
-    setTimeout(function() {
-      var slide = $('#slide-2');
-      slide.show().children().css({'visibility':'hidden'}).removeClass('animated');
-      slide.find('h1').css({'visibility':'visible'}).addClass('animated bounceIn');
-      setTimeout(function(){
-        slide.find('h1, img').css({'visibility':'visible'}).addClass('animated bounceIn');
-        setTimeout(function(){
-          slide.find('.side').css({'visibility':'visible'}).addClass('animated slideInRight');
-        }, 2000);
-      }, 3000);
-    }, 1600);
+    showSlide2();
   });
 
   $('.a-3').on('click', function(){
-    setTimeout(function() {
-      var slide1 = $('#slide-3');
-      var slide2 = $('#slide-4');
-
-      slide1.show().children().css({'visibility':'hidden'}).removeClass('animated');
-      slide2.show().children().css({'visibility':'hidden'}).removeClass('animated');
-      slide1.find('h1').css({'visibility':'visible'}).addClass('animated bounceIn');
-      setTimeout(function(){
-        slide1.find('h1, img').css({'visibility':'visible'}).addClass('animated bounceIn');
-        setTimeout(function(){
-          slide1.find('.side').css({'visibility':'visible'}).addClass('animated slideInLeft');
-          setTimeout(function(){
-            slide1.show().children().css({'visibility':'hidden'}).removeClass('animated');
-            slide2.find('h1').css({'visibility':'visible'}).addClass('animated bounceIn');
-            setTimeout(function(){
-              slide2.find('img').css({'visibility':'visible'}).addClass('animated bounceIn');
-              setTimeout(function(){
-                slide2.find('.side').css({'visibility':'visible'}).addClass('animated slideInRight');
-                slide2.find('p.center').css({'visibility':'visible'}).addClass('animated bounceInUp');
-              }, 2000);
-            }, 2000);
-          }, 3000);
-        }, 2000);
-      }, 3000);
-    }, 1600);
+    showSlide3();
   });
 
   $('.a-4').on('click', function(){
-    setTimeout(function() {
-      var slide = $('#slide-5');
-      slide.show().children().css({'visibility':'hidden'}).removeClass('animated');
-      slide.find('h1').css({'visibility':'visible'}).addClass('animated bounceIn');
-      setTimeout(function(){
-        slide.find('h1, img').css({'visibility':'visible'}).addClass('animated bounceIn');
-        setTimeout(function(){
-          slide.find('.side').css({'visibility':'visible'}).addClass('animated slideInLeft');
-          slide.find('p.center').css({'visibility':'visible'}).addClass('animated bounceInUp');
-        }, 2000);
-      }, 3000);
-    }, 1600);
+    showSlide4();
   });
 
-   $('.a-5').on('click', function(){
-    setTimeout(function() {
-      var slide = $('#slide-6');
-      slide.show().children().css({'visibility':'hidden'}).removeClass('animated');
-      slide.find('h1').css({'visibility':'visible'}).addClass('animated bounceIn');
-      setTimeout(function(){
-        slide.find('h2').css({'visibility':'visible'}).addClass('animated bounceIn');
-        setTimeout(function(){
-          slide.find('video').css({'visibility':'visible'}).addClass('animated bounceInUp');
-        }, 1000);
-      }, 1000);
-    }, 1600);
+  $('.a-5').on('click', function(){
+    showSlide5();
   });
+
+  function resetAll() {
+    $('video')[0].pause();
+    clearTimeout(timer);
+    $('.slide').hide();
+    $('.slide').finish();
+    $('.slide, .slide *').finish(true, true).removeClass('animated');
+  }
+
+  function showSlide1() {
+    var slide = $('#slide-1');
+
+    slide.show().children().hide().delay(1000);
+
+    slide.children('h1.center').delay(400).show(0).addClass('animated zoomIn');
+    slide.children('h1 + p').delay(2500).show(0).addClass('animated zoomIn');
+    slide.children('.img-1').delay(3000).show(0).addClass('animated zoomIn');
+    slide.children('.img-2').delay(3500).show(0).addClass('animated zoomIn');
+    slide.children('.img-3').delay(4000).show(0).addClass('animated zoomIn');
+    slide.children('.img-4').delay(4500).show(0).addClass('animated zoomIn');
+    slide.children('h2').delay(6000).show(0).addClass('animated zoomIn');
+    slide.children('.minsk').delay(6500).show(0).addClass('animated slideInUp');
+    slide.children('.lib').delay(7000).show(0).addClass('animated slideInUp');
+  }
+
+  function showSlide2() {
+    var slide = $('#slide-2');
+
+    slide.show().children().hide().delay(1000);
+    slide.children('h1.center').delay(400).show(0).addClass('animated zoomIn');
+    slide.children('.img-1').delay(2500).show(0).addClass('animated zoomIn');
+    slide.children('.img-2').delay(3000).show(0).addClass('animated zoomIn');
+    slide.children('.img-3').delay(3500).show(0).addClass('animated zoomIn');
+    slide.children('.img-4').delay(4000).show(0).addClass('animated zoomIn');
+    slide.children('.side').delay(5500).show(0).find('*').addClass('animated slideInRight');
+  }
+
+  function showSlide3() {
+    var slide1 = $('#slide-3');
+    var slide2 = $('#slide-4');
+
+    slide1.show().children().hide().delay(1000);
+    slide1.children('h1.center').delay(400).show(0).addClass('animated zoomIn');
+    slide1.children('.img-1').delay(2500).show(0).addClass('animated zoomIn');
+    slide1.children('.img-2').delay(3000).show(0).addClass('animated zoomIn');
+    slide1.children('.img-3').delay(3500).show(0).addClass('animated zoomIn');
+    slide1.children('.img-4').delay(4000).show(0).addClass('animated zoomIn');
+    slide1.children('.side').delay(2000).show(0).find('*').addClass('animated slideInLeft');
+    slide1.delay(10000).hide(0);
+    slide2.show().children().hide().delay(1000);
+    slide2.children('h1.center').delay(10400).show(0).addClass('animated zoomIn');
+    slide2.children('.side').delay(14000).show(0).find('*').addClass('animated slideInRight');
+    slide2.children('.img-1').delay(12500).show(0).addClass('animated zoomIn');
+    slide2.children('.img-2').delay(13000).show(0).addClass('animated zoomIn');
+    slide2.children('p.center').delay(15500).show(0).addClass('animated slideInUp');
+  }
+
+  function showSlide4() {
+    var slide = $('#slide-5');
+
+    slide.show().children().hide().delay(1000);
+    slide.children('h1.center').delay(400).show(0).addClass('animated zoomIn');
+    slide.children('.img-1').delay(2500).show(0).addClass('animated zoomIn');
+    slide.children('.img-2').delay(3000).show(0).addClass('animated zoomIn');
+    slide.children('.img-3').delay(3500).show(0).addClass('animated zoomIn');
+    slide.children('.side').delay(5500).show(0).addClass('animated fadeIn');
+  }
+
+  function showSlide5() {
+    var slide = $('#slide-6');
+
+    slide.show().children().hide().delay(1000);
+    slide.children('h1.center').delay(400).show(0).addClass('animated zoomIn');
+    slide.children('h2').delay(2500).show(0).addClass('animated zoomIn');
+    slide.children('video').delay(3500).show(0).addClass('animated bounceInUp');
+  }
 
 });
